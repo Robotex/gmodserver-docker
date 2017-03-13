ae8a42fa92431e4098792460381a1f17f18e0cf3
Robotex <robotex@altervista.org>
2017-03-13 01:23:57 +0100

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,23 +3,20 @@ FROM renshou/steamcmd:latest
 # Prevent some warnings
 ARG DEBIAN_FRONTEND=noninteractive
 
-# Folder name (must be same as update script)
-ENV GAME=gmod
-
 # Default port binding
 ENV BIND_PORT=27015
 ENV BIND_IP=0.0.0.0
 
 # Copy scripts
-COPY update.txt /srv/${GAME}/update.txt
-COPY start.sh /srv/${GAME}/start.sh
+COPY update.txt /srv/srcds/update.txt
+COPY start.sh /srv/srcds/start.sh
 
 # Install dependencies & assign files ownership
 RUN apt-get update \
     && apt-get -y install lib32ncurses5 \
     && rm -rf /var/lib/apt/lists/* \
-    && chown gameserver:gameserver /srv/${GAME}/update.txt /srv/${GAME}/start.sh \
-    && chmod +x /srv/${GAME}/start.sh
+    && chown gameserver:gameserver /srv/srcds/update.txt /srv/srcds/start.sh \
+    && chmod +x /srv/srcds/start.sh
 
 # Switch to non root user
 USER gameserver
--- start.sh
+++ start.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
-if [ ! -f /srv/$GAME/serverfiles/srcds_run ]
+if [ ! -f /srv/srcds/serverfiles/srcds_run ]
 then
     echo "Executable not found!"
     exit;
 fi
 
-if [ ! -f /srv/${GAME}/addons/.speranza ]
+if [ ! -f /srv/srcds/addons/.speranza ]
 then
-    if [ ! -L /srv/${GAME}/serverfiles/garrysmod/addons ]
+    if [ ! -L /srv/srcds/serverfiles/garrysmod/addons ]
     then
-        cp -r /srv/${GAME}/serverfiles/garrysmod/addons/* /srv/${GAME}/addons
-        mv /srv/${GAME}/serverfiles/garrysmod/addons /srv/${GAME}/serverfiles/garrysmod/addons_backup
-        ln -s /srv/${GAME}/addons /srv/${GAME}/serverfiles/garrysmod
+        cp -r /srv/srcds/serverfiles/garrysmod/addons/* /srv/srcds/addons
+        mv /srv/srcds/serverfiles/garrysmod/addons /srv/srcds/serverfiles/garrysmod/addons_backup
+        ln -s /srv/srcds/addons /srv/srcds/serverfiles/garrysmod
     fi
 
-    touch /srv/${GAME}/addons/.speranza
+    touch /srv/srcds/addons/.speranza
 fi
 
-if [ ! -f /srv/${GAME}/cfg/.speranza ]
+if [ ! -f /srv/srcds/cfg/.speranza ]
 then
-    if [ ! -L /srv/${GAME}/serverfiles/garrysmod/cfg ]
+    if [ ! -L /srv/srcds/serverfiles/garrysmod/cfg ]
     then
-        cp -r /srv/${GAME}/serverfiles/garrysmod/cfg/* /srv/${GAME}/cfg
-        mv /srv/${GAME}/serverfiles/garrysmod/cfg /srv/${GAME}/serverfiles/garrysmod/cfg_backup
-        ln -s /srv/${GAME}/cfg /srv/${GAME}/serverfiles/garrysmod
+        cp -r /srv/srcds/serverfiles/garrysmod/cfg/* /srv/srcds/cfg
+        mv /srv/srcds/serverfiles/garrysmod/cfg /srv/srcds/serverfiles/garrysmod/cfg_backup
+        ln -s /srv/srcds/cfg /srv/srcds/serverfiles/garrysmod
     fi
 
-    touch /srv/${GAME}/cfg/.speranza
+    touch /srv/srcds/cfg/.speranza
 fi
 
-/srv/$GAME/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
+/srv/srcds/serverfiles/srcds_run -ip $BIND_IP -port $BIND_PORT -strictportbind "$@"
