904b46f20f5843d138b74a97546a982dda99331e
Robotex <robotex@altervista.org>
2017-04-02 21:28:29 +0200

    Update cfg

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,9 +6,35 @@ then
     exit;
 fi
 
-/usr/games/gameserver/serverfiles/srcds_run -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -strictportbind "$@" -norestart -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \"${SRCDS_HOSTNAME}\" &
+_trapTERM() {
+    echo "---TERM signal received!---"
+    kill -15 "$childpid" 2>/dev/null
+}
+
+_trapINT() {
+    echo "---INT signal signal!---"
+    kill -2 "$childpid" 2>/dev/null
+}
+
+_trapHUP() {
+    echo "---HUP signal signal!---"
+    kill -1 "$childpid" 2>/dev/null
+}
+
+_trapUSR1() {
+    echo "---USR1 signal signal!---"
+    kill -10 "$childpid" 2>/dev/null
+}
+
+rm /var/lock/gameserver.lock || true
+
+trap _trapTERM 15
+
+/usr/games/gameserver/serverfiles/srcds_run -game $SRCDS_GAME -ip $BIND_IP -port $BIND_PORT -pingboost 2 -timeout 0 -strictportbind "$@" -norestart -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \"${SRCDS_HOSTNAME}\" &
 
 childpid=$! 
 while wait $childpid; test $? -gt 128; do
     kill -0 $childpid 2> /dev/null || break;
 done
+
+trap - 15
