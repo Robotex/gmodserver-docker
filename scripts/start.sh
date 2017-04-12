36ffef9ab322a6f9c46b489750504c7c540107b6
Robotex <robotex@altervista.org>
2017-04-13 00:16:32 +0200

    Update start params

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,7 +30,7 @@ rm /var/lock/gameserver.lock || true
 
 trap _trapTERM 15
 
-/usr/games/gameserver/serverfiles/srcds_run -game "${SRCDS_GAME}" -strictportbind -ip "${BIND_IP}" -port "${BIND_PORT}" +clientport "${SRCDS_CLIENT_PORT}" +tv_port "${SRCDS_TV_PORT}" +host_workshop_collection "${WORKSHOP_COLLECTION}" -authkey "${AUTHKEY}" -pingboost 2 -timeout 0 +exec server/autoexec.cfg -norestart -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname "${SRCDS_HOSTNAME}" "$@" &
+/usr/games/gameserver/serverfiles/srcds_run -game "${SRCDS_GAME}" -strictportbind -ip "${BIND_IP}" -port "${BIND_PORT}" +clientport "${SRCDS_CLIENT_PORT}" +tv_port "${SRCDS_TV_PORT}" +host_workshop_collection "${WORKSHOP_COLLECTION}" -authkey "${AUTHKEY}" -pingboost 2 -timeout 0 +exec server/autoexec.cfg -norestart -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \""${SRCDS_HOSTNAME}"\" "$@" &
 
 childpid=$! 
 while wait $childpid; test $? -gt 128; do
