2488530ddaaea4a80dc6324c77db6743de604b10
Robotex <robotex@altervista.org>
2017-04-13 01:10:50 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,35 +6,4 @@ then
     exit;
 fi
 
-_trapTERM() {
-    echo "---TERM signal received!---"
-    kill -15 "$childpid" 2>/dev/null
-}
-
-_trapINT() {
-    echo "---INT signal signal!---"
-    kill -2 "$childpid" 2>/dev/null
-}
-
-_trapHUP() {
-    echo "---HUP signal signal!---"
-    kill -1 "$childpid" 2>/dev/null
-}
-
-_trapUSR1() {
-    echo "---USR1 signal signal!---"
-    kill -10 "$childpid" 2>/dev/null
-}
-
-rm /var/lock/gameserver.lock || true
-
-trap _trapTERM 15
-
-/usr/games/gameserver/serverfiles/srcds_run -game "${SRCDS_GAME}" -strictportbind -ip "${BIND_IP}" -port "${BIND_PORT}" +clientport "${SRCDS_CLIENT_PORT}" +tv_port "${SRCDS_TV_PORT}" +host_workshop_collection "${WORKSHOP_COLLECTION}" -authkey "${AUTHKEY}" -pingboost 2 -timeout 0 +exec server/autoexec.cfg -norestart -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \""${SRCDS_HOSTNAME}"\" "$@" 2>&1 &
-
-childpid=$! 
-while wait $childpid; test $? -gt 128; do
-    kill -0 $childpid 2> /dev/null || break;
-done
-
-trap - 15
+/usr/games/gameserver/serverfiles/srcds_run -game "${SRCDS_GAME}" -strictportbind -ip "${BIND_IP}" -port "${BIND_PORT}" +clientport "${SRCDS_CLIENT_PORT}" +tv_port "${SRCDS_TV_PORT}" +host_workshop_collection "${WORKSHOP_COLLECTION}" -authkey "${AUTHKEY}" -pingboost 2 -timeout 0 +exec server/autoexec.cfg -autoupdate -steam_dir ~/.steam/steamcmd -steamcmd_script /usr/games/gameserver/update.txt +hostname \""${SRCDS_HOSTNAME}"\" "$@"
